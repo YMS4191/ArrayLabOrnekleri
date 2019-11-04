8587c98f733b70fed96574df545d9493b6c1840b
muratvuranok <muratvuranok@hotmail.com>
2019-11-04 12:51:59 +0300

    Add project files.

---
+++ ArrayLabOrnekleri/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ArrayLabOrnekleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrnek1 = new System.Windows.Forms.Button();
+            this.txtGelenDeger = new System.Windows.Forms.TextBox();
+            this.btnOrnek2 = new System.Windows.Forms.Button();
+            this.lstElemanlar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOrnek1
+            // 
+            this.btnOrnek1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek1.Location = new System.Drawing.Point(13, 13);
+            this.btnOrnek1.Name = "btnOrnek1";
+            this.btnOrnek1.Size = new System.Drawing.Size(274, 50);
+            this.btnOrnek1.TabIndex = 0;
+            this.btnOrnek1.Text = "Örnek";
+            this.btnOrnek1.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Click += new System.EventHandler(this.btnOrnek1_Click);
+            // 
+            // txtGelenDeger
+            // 
+            this.txtGelenDeger.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGelenDeger.Location = new System.Drawing.Point(13, 69);
+            this.txtGelenDeger.Name = "txtGelenDeger";
+            this.txtGelenDeger.Size = new System.Drawing.Size(274, 38);
+            this.txtGelenDeger.TabIndex = 1;
+            // 
+            // btnOrnek2
+            // 
+            this.btnOrnek2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek2.Location = new System.Drawing.Point(13, 113);
+            this.btnOrnek2.Name = "btnOrnek2";
+            this.btnOrnek2.Size = new System.Drawing.Size(274, 50);
+            this.btnOrnek2.TabIndex = 2;
+            this.btnOrnek2.Text = "Eleman Ekle";
+            this.btnOrnek2.UseVisualStyleBackColor = true;
+            this.btnOrnek2.Click += new System.EventHandler(this.btnOrnek2_Click);
+            // 
+            // lstElemanlar
+            // 
+            this.lstElemanlar.FormattingEnabled = true;
+            this.lstElemanlar.Location = new System.Drawing.Point(13, 172);
+            this.lstElemanlar.Name = "lstElemanlar";
+            this.lstElemanlar.Size = new System.Drawing.Size(274, 264);
+            this.lstElemanlar.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 450);
+            this.Controls.Add(this.lstElemanlar);
+            this.Controls.Add(this.btnOrnek2);
+            this.Controls.Add(this.txtGelenDeger);
+            this.Controls.Add(this.btnOrnek1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrnek1;
+        private System.Windows.Forms.TextBox txtGelenDeger;
+        private System.Windows.Forms.Button btnOrnek2;
+        private System.Windows.Forms.ListBox lstElemanlar;
+    }
+}
+
---
+++ ArrayLabOrnekleri/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ArrayLabOrnekleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrnek1_Click(object sender, EventArgs e)
+        {
+            //Icerigine sizin karar vereceginiz bir sayisal dizi olusturun ve bu dizinin en kucuk - en buyuk elemanını MessageBox'la gosterin..
+            int[] dizi = { 53, 765, 2165, 8, 321, 65, 851, 876, 98, 34276, 453 };
+            Array.Sort(dizi);
+            MessageBox.Show($"Dizinin ilk elemanı : {dizi[0]}\nDizinin son elemanı : {dizi[dizi.Length - 1]}");
+        }
+
+
+        string[] kayitlar = new string[0];
+        //int adet = 1;
+        //int index = 0;
+        private void btnOrnek2_Click(object sender, EventArgs e)
+        {
+            //Textbox'tan girilen veri "kayitlar" adli diziye eklesin. Her yeni kayıtta dizinin boyutu bir arttırılsın... Eleman ekledikten hemen sonra ise asagidaki formatta kayit listbox'ta gosterilsin..
+            //FORMAT => Eleman - ElemaninIndex
+
+            //string veri = txtGelenDeger.Text;
+            //Array.Resize(ref kayitlar, adet);
+
+            //kayitlar[index] = veri;
+            //adet++;
+            //index++;
+
+            //lstElemanlar.Items.Add($"{veri} - {index}");
+
+            Array.Resize(ref kayitlar, kayitlar.Length + 1);
+            kayitlar[kayitlar.Length - 1] = txtGelenDeger.Text;
+            lstElemanlar.Items.Add($"{kayitlar[kayitlar.Length - 1]} - {kayitlar.Length - 1}");
+        }
+    }
+}
